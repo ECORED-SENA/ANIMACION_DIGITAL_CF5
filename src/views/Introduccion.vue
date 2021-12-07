e59927a93e8a3872ff37ba5f3ba3332dcedbbf60
Luis Urueta <lgurueta@misena.edu.co>
2021-12-06 21:11:30 -0500

    Avance02

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,29 +1,30 @@
 <template lang="pug">
 .curso-main-container.introduccion
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.BRcardTOP.bg-white.p-4.p-md-5
   
-    .titulo-principal
+    .titulo-principal.mb-0
       .titulo-principal__numero
         span
           i.fas.fa-info
       h1 Introducción
-    
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
-    
-    p.mb-5 La necesidad de mitigar la pandemia por COVID-19 ha obligado a la comunidad científica internacional, a desarrollar una serie de investigaciones y desarrollos tendientes a presentar una vacuna para hacerle frente a esta enfermedad que aumenta día a día su morbi mortalidad en Colombia. Es así, como en diciembre de 2020, la comunidad científica presenta los resultados de varios desarrollos de vacunas, que ofrecen eficacia y seguridad, respaldados por estudios clínicos rigurosos.
+  
+  .container.p-4.p-md-5
 
-    figure.mb-5
-      img(src="@/assets/curso/banner-intro.jpg", alt="Texto que describa la imagen")
-      figcaption Imagen 1. Vacunación
+    .row.bg-white.BRcard.Brd01.p-4.py-5.justify-content-between
+      .col-lg-6
+        figure.mb-4
+          img.maxW01(src='@/assets/curso/intro/03.jpg', alt='Logo AE')
+        p Una de las áreas más comunes de interés para los diseñadores de movimientos es la animación de personajes. ¿Cuándo chico le atraían los detrás de cámaras y la forma de trabajar de los estudios Disney? O, ¿quizá requiere hacer más interesante un video de presentación para un producto o servicio? Sin importar la razón, algo es cierto: la animación de personajes es compleja, pero no difícil de lograr. Requiere mucha práctica, tiempo y conocimiento tanto de la herramienta digital como del funcionamiento del sistema que se quiera animar, en este caso un ser antropomorfo, además de un alto nivel de análisis, no solo del funcionamiento y dinámicas de cuerpo humano sino también de la lógica e influencia de la física del ambiente para generar una animación convincente dependiendo del nivel de realismo o apariencia dinámica que se le quiera dar al personaje y, finalmente, video.  
+        p.mb-5 Sin embargo, antes de que puedas comenzar a practicar para aprender la animación de personajes, debes asegurarte de que estás usando las herramientas adecuadas. Es por esto, que en esta etapa veremos una descripción general rápida y divertida de las herramientas de manipulación de personajes desde After Effects.
+        figure
+          img.maxW02(src='@/assets/curso/intro/01.jpg', alt='Logo Adobe')
+      .col-lg-5
+        figure
+          img(src='@/assets/curso/intro/02.jpg', alt='Decoración')
 
-    p.mb-0 La llegada de nuevas vacunas al país supone un reto para el talento humano en salud responsable de la vacunación y desarrollar habilidades para ofrecer una orientación oportuna y segura a las personas objeto de la vacuna.
-      br
-      br
-      | En ese orden de ideas, este componente formativo guía al aprendiz hacia la identificación de los aspectos normativos, técnicos y operativos de la vacunación contra COVID19 en Colombia y de las generalidades de la infección por SARS-2 COVID 19.
+    
+    
 
 </template>
 
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -1,24 +1,87 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+  .container.BRcardTOP.bg-white.p-4.p-md-5
 
-    //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
-    //- se encuentra en src/components/Muestras.vue
-    //- Una vez el componente "Muestras" no se necesite 
-    //- Se debe borrar el "import Muestras from '../components/Muestras'" y en "components" en "<script"
-    //- esto evitará que se compile en la carpeta final de distribución
-    Muestras
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 1
+      h1 Fundamentación
+
+    .row.justify-content-center
+      .col-md-10
+        p El proceso de un buen rigging lleva tiempo y cuanto más tiempo se pase organizando y enlazando los elementos del sistema, más tiempo se habrá ganado a la hora de animar. Como resultado, muchas herramientas de animación de personajes cuentan con opciones de enlace automáticas que agilizan este proceso de enlace. Algunas de las herramientas requieren que configure el personaje y presione un solo botón para activar automáticamente un complejo proceso de manipulación. 
+      
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/tema1/01.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 Y si bien el proceso en After Effects y en animación 2D es un poco más sencillo que en la animación 3D, también hay que tener en cuenta los principios básicos de animación y lógica que aplican a cualquier tipo de movimiento. A veces, se puede estar trabajando con un personaje simple que posee pocos puntos animados y otras puede ser mucho más complejo. Si planea trabajar en proyectos de mayor detalle y realismo, se debe asegurar que la herramienta de manipulación de personajes tenga alcances de automatización para que funcione en un flujo de trabajo más grande si es necesario.
+  
+  .container.LnrGrad01.p-4.p-md-5
+
+    #t1_1.titulo-segundo.color-secundario
+      h2 1.1 Antes del #[em rigging]
+    
+    p.mb-4 El rigging es una parte vital del flujo de trabajo de la animación. Sin rig no habría forma de animar a los personajes. Como “aparejador” o rigger, la labor es crear controles flexibles e intuitivos que puedan lograr cualquier animación que se les presente. En el caso de After Effects y sus herramientas nativas, solo es cuestión de conocer bien su manejo y saber dónde generar los anclajes y pivotes en el personaje.
+
+    .row.justify-content-center.mb-4
+      .col-md-6.bg-white.BR01.Brd02.p-3
+        p.text-center.mb-0 A continuación, se detallan algunos elementos primordiales: 
+    
+    TabsA.color-acento-botones
+      .tarjeta.LnrGrad02.p-4(titulo="Generar deformaciones limpias")
+        .row.align-items-center
+          .col-lg-6
+            p Este es probablemente uno de los aspectos más importantes para tener un excelente rig de personaje. Este se refiere a que el sistema necesita deformarse apropiadamente y creíble en cada una de las áreas de manipulación. Por ejemplo, si un personaje se inclina, el estómago y pecho se deben deformar de manera elástica y natural.
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/03.svg', alt='Decoración')
+      
+      .tarjeta.LnrGrad02.p-4(titulo="Controles claros de <em>rig</em>")
+        .row.align-items-center
+          .col-lg-6
+            p Para que un animador mueva las articulaciones individuales de un personaje, necesitará tener acceso a los controles para que el proceso de selección sea mucho más fácil. La ubicación de estos controles debe quedar clara en la plataforma, el animador debería poder decir exactamente qué parte del cuerpo influenciará determinado control sin tener que seleccionarla primero.
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/02.svg', alt='Decoración')
+        
+      .tarjeta.LnrGrad02.p-4(titulo="Correcto despiece de personaje")
+        .row.align-items-center
+          .col-lg-6
+            p Es de suma importancia tener una imagen correctamente despiezada para no incurrir en tensiones de controles erróneas sobre partes del cuerpo que no deban ser afectadas. Para ello, es indispensable que cada articulación esté independiente en su propia capa o, en caso de estar en una misma capa, que no esté cerca o cruzándose con otra. Un buen ejemplo es la posición del hombre de Vitruvio de Da Vinci.
+          .col-lg-6
+            figure
+              img(src='@/assets/curso/tema1/04.svg', alt='Decoración')
+  
+  .container.BRcardTOP.bg-white.px-4.px-md-5.pt-0.pb-4
+
+    p.text-center Ahora, se revisará el despiece del personaje a través del siguiente video:
+
+    figure
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    
+    separador
+
+    #t1_2.titulo-segundo.color-secundario
+      h2 1.2 Sistema de controles
+    
+    p.mb-4 De forma nativa en After Effects podemos encontrar dos herramientas o sistemas que enlace que nos facilitan de forma práctica, rápida e intuitiva el enlace, parentazgo o rigging de personajes 2D. También existen extensiones de terceros al programa, unos libres y otros de pago en caso de requerir herramientas más complejas y de altos estándares competitivos en el mercado. Sin embargo, siempre lo más importante es la habilidad del animador para usarlas, sin importar si son pagas o nativas. Estas son:
+
+
+
+
+
+
   
 </template>
 
 <script>
-import Muestras from '../components/Muestras' // borrar una vez el componente "Muestras" no se necesite
 export default {
   name: 'Tema1',
-  components: {
-    Muestras, // borrar una vez el componente "Muestras" no se necesite
-  },
+  components: {},
   data: () => ({
     // variables de vue
   }),
